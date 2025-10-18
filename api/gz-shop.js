4920da3a8c458345a4241526a67243203d93f5d6
longmai96981 <13579014@qq.com>
2025-10-18 09:22:06 +0800

    Create gz-shop.js

---
+++ api/gz-shop.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { areaId = 440100, page = 1 } = req.query;
+  const { data } = await axios.get('https://www.pubangwang.com/api/shop/search', {
+    params: { areaId, industry: 0, area: 0, page },
+    headers: { 'User-Agent': 'Mozilla/5.0' },
+    timeout: 8000,
+  });
+  res.status(200).json(data);
+};
